e397ba14efbfe668ba5dd3edc23b8e5936cf9178
mrizkinash <mrizkinash@gmail.com>
2023-02-27 20:45:21 +0700

    Added cart context and chocolate card

---
+++ src/components/ChocolateCard.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { useCart } from "../hooks/useCart";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Input from '@mui/material/Input';
+
+export function ChocolateCard(chocolate) {
+    const { addItemToCart } = useCart();
+
+    const [quantity, setQuantity] = useState(1);
+
+    function handleIncrease() {
+        setQuantity((state) => state + 1);
+    }
+
+    function handleDecrease() {
+        setQuantity((state) => state - 1);
+    }
+
+    function handleAddToCart() {
+        const toAdd = {
+            ...chocolate,
+            quantity,
+        };
+
+        addItemToCart(toAdd);
+
+        setQuantity(1);
+    }
+
+    return (
+        <Container>
+            <Card
+                sx={{
+                    height: '100%', 
+                    display: 'flex', 
+                    flexDirection: 'column',
+                }}
+            >
+                <CardMedia
+                    component="img" 
+                    image={chocolate.img}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {chocolate.name}
+                    </Typography>
+                    <Typography>
+                        {chocolate.desc}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" onClick={handleDecrease}>
+                        <RemoveIcon />
+                    </Button>
+                    <Input 
+                        inputProps={{ 
+                            inputMode: 'numeric', 
+                            pattern: '[0-9]*' 
+                        }} 
+                        readOnly='true' 
+                        value={quantity} 
+                    />
+                    <Button size="small" onClick={handleIncrease}>
+                        <AddIcon />
+                    </Button>
+                    <Button size="small" onClick={handleAddToCart}>
+                        <AddShoppingCartIcon />
+                    </Button>
+                </CardActions>
+            </Card>
+        </Container>
+    )
+}
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,87 @@
+import { createContext, useEffect, useState } from 'react'
+
+const STORAGE_KEY = 'Valentinus';
+export const INCREASE_ITEM = '+';
+export const DECREASE_ITEM = '-';
+
+export const CartContext = createContext({});
+
+export function CartContextProvider(children) {
+    const [cartItems, setCartItems] = useState(() => {
+        const cartCache = localStorage.getItem(STORAGE_KEY);
+
+        if (cartCache) {
+            return JSON.parse(cartCache);
+        }
+
+        return [];
+    });
+
+    const cartQuantity = cartItems.length;
+    const cartTotalPrice =  cartItems.reduce((total, item) => {
+        return total + item.price * item.quantity;
+    }, 0);
+
+    function addItemToCart(item) {
+        const itemIsInCart = cartItems.findIndex((cartItem) => cartItem.id === item.id);
+        const newCart = cartItems.slice();
+
+        if (itemIsInCart < 0) {
+            newCart.push(item);
+        } else {
+            newCart[itemIsInCart].quantity += item.quantity;
+        }
+
+        setCartItems(newCart);
+    }
+
+    function changeCartItemQuantity(cartItemId, action){
+        const itemIsInCart = cartItems.findIndex((cartItem) => cartItem.id === cartItemId);
+        const newCart = cartItems.slice();
+
+        if (itemIsInCart >= 0) {
+            if (action === INCREASE_ITEM) {
+                newCart[itemIsInCart].quantity += 1;
+            } else if (action === DECREASE_ITEM) {
+                newCart[itemIsInCart].quantity -= 1;
+            }
+        }
+
+        setCartItems(newCart);
+    }
+
+    function removeCartItem(cartItemId) {
+        const itemIsInCart = cartItems.findIndex((cartItem) => cartItem.id === cartItemId);
+        const newCart = cartItems.slice();
+
+        if (itemIsInCart >= 0) {
+            newCart.splice(itemIsInCart, 1);
+        }
+
+        setCartItems(newCart);
+    }
+
+    function clearCart() {
+        setCartItems([]);
+    }
+
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(cartItems))
+    }, [cartItems])
+
+    return (
+        <CartContextProvider
+            value={{
+                cartItems,
+                addItemToCart,
+                cartQuantity,
+                cartTotalPrice,
+                changeCartItemQuantity,
+                removeCartItem,
+                clearCart,
+            }}
+        >
+            {children}
+        </CartContextProvider>
+    )
+}
